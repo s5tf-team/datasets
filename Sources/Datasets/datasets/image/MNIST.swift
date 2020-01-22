59da93e3842f72ff496889c77e8a68350d22d4d6
Rick Wierenga <rick_wierenga@icloud.com>
2020-01-22 09:10:08 +0100

    fix access control

--- Sources/Datasets/datasets/image/MNIST.swift
+++ Sources/Datasets/datasets/image/MNIST.swift
@@ -1,27 +1,27 @@
-struct MNISTBatch: S5TFLabeledBatch {
-    var data: Int
-    var labels: Int
+public struct MNISTBatch: S5TFLabeledBatch {
+    public var data: Int
+    public var labels: Int
 }
 
-struct MNIST: S5TFDataset {
+public struct MNIST: S5TFDataset {
     typealias _Iterator = MNISTIterator // swiftlint:disable:this type_name
-    let data = [[1, 2], [3, 4], [5, 6]]
-    func batched(_ batchSize: Int) -> MNISTIterator {
+    internal let data = [[1, 2], [3, 4], [5, 6]]
+    public func batched(_ batchSize: Int) -> MNISTIterator {
         return MNISTIterator(dataset: self)
     }
 }
 
-struct MNISTIterator: S5TFDatasetIterator {
+public struct MNISTIterator: S5TFDatasetIterator {
     typealias _Dataset = MNIST // swiftlint:disable:this type_name
-    var dataset: MNIST
+    private var dataset: MNIST
 
     private var index = 0
 
-    init(dataset: MNIST) {
+    public init(dataset: MNIST) {
         self.dataset = dataset
     }
 
-    mutating func next() -> MNISTBatch? {
+    mutating public func next() -> MNISTBatch? {
         guard index < dataset.data.count else { return nil }
         let data = dataset.data[index]
         index += 1
