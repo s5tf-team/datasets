fbf9f94400a01a44f25362cd87ab928b89a37c59
Rick Wierenga <rick_wierenga@icloud.com>
2020-01-23 00:08:06 +0100

    specify local path

--- Sources/Datasets/Downloader.swift
+++ Sources/Datasets/Downloader.swift
@@ -5,7 +5,8 @@ public class Downloader: NSObject {
     private lazy var session = URLSession(configuration: .default,
                                           delegate: self,
                                           delegateQueue: nil)
-    private var completionHandler: ((URL?, Error?) -> Void)? = nil
+    private var localPath: String?
+    private var completionHandler: ((URL?, Error?) -> Void)?
     private var semaphore: DispatchSemaphore?
     private var startingTime: Date?
 
@@ -13,13 +14,29 @@ public class Downloader: NSObject {
     ///
     /// - Parameters:
     ///   - `fileAt`: the remote url
-    ///   - `completionHandler`: will be called upon completion. First item might be the local URL,
-    ///                          second item might be an error.
-    public func download(fileAt url: URL, completionHandler: @escaping (URL?, Error?) -> Void) {
+    ///   - `to`: the local path
+    ///   - `completionHandler`: will be called upon completion. First item might be the local path,
+    ///                          second item might be an error. If the item can't be saved to the local
+    ///                          url an error will be returned.
+    public func download(fileAt remoteUrl: URL,
+                         to localPath: String,
+                         completionHandler: @escaping (URL?, Error?) -> Void) {
+        self.localPath = localPath
+        download(fileAt: remoteUrl, completionHandler: completionHandler)
+    }
+
+    /// Downloads a file.
+    ///
+    /// - Parameters:
+    ///   - `fileAt`: the remote url
+    ///   - `completionHandler`: will be called upon completion. First item might be the local path,
+    ///                          second item might be an error. If the item can't be saved to the local
+    ///                          url an error will be returned.
+    public func download(fileAt remoteUrl: URL, completionHandler: @escaping (URL?, Error?) -> Void) {
         // Start a new download task. Use a semaphore to wait for the download progress to finish
         // before we continue execution.
         semaphore = DispatchSemaphore(value: 0)
-        session.downloadTask(with: url).resume()
+        session.downloadTask(with: remoteUrl).resume()
         self.completionHandler = completionHandler
         startingTime = Date()
         semaphore!.wait()
@@ -61,17 +78,32 @@ extension Downloader: URLSessionDownloadDelegate {
                            downloadTask: URLSessionDownloadTask,
                            didFinishDownloadingTo location: URL) {
         print("\n") // Keep the progress bar.
-        startingTime = nil
-        completionHandler?(location, nil)
         semaphore?.signal()
+        startingTime = nil
+
+        // Move the file to the desired local URL.
+        do {
+            let documentsURL = try
+            FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+            if localPath == nil { localPath = location.lastPathComponent}
+            let savedURL = documentsURL.appendingPathComponent(localPath!)
+            completionHandler?(savedURL, nil)
+            try FileManager.default.moveItem(at: location, to: savedURL)
+            localPath = nil
+        } catch {
+            completionHandler?(nil, error)
+        }
     }
 
     public func urlSession(_ session: URLSession,
                            task: URLSessionTask,
                            didCompleteWithError error: Error?) {
         print("\n") // Keep the progress bar.
+        semaphore?.signal()
         startingTime = nil
         completionHandler?(nil, error)
-        semaphore?.signal()
     }
 }
