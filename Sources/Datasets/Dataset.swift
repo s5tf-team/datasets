14e3cd5a3de78718ade1d7d99f5e0916fad2c8b9
Rick Wierenga <rick_wierenga@icloud.com>
2020-01-24 21:04:22 +0100

    make changes requested in the PR

--- Sources/Datasets/Dataset.swift
+++ Sources/Datasets/Dataset.swift
@@ -2,14 +2,12 @@ import Foundation
 
 protocol S5TFDataLoader: Sequence, IteratorProtocol {
     func batched(_ batchSize: UInt) -> Self
-    func shuffled() -> Self
 }
 
 protocol S5TFDataset where DataLoader: S5TFDataLoader {
     associatedtype DataLoader
     static var info: S5TFDatasetInfo { get }
     static var train: DataLoader { get }
-    static var validation: DataLoader { get }
 }
 
 public struct S5TFDatasetInfo {
